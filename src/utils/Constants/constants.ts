e8a6b1b4f1a84362163a0cfc5a6c0500aed5623d
KseniyaEfimova <xkseefi@gmail.com>
2024-07-12 16:26:07 +0300

    added api consts

---
+++ src/utils/Constants/constants.ts
@@ -0,0 +1,5 @@
+const SWAPI_URL: string = 'https://swapi.dev/api/';
+const CHARACTERS: string = 'people';
+export const IMAGE_CHARS_URL: string =
+  'https://starwars-visualguide.com/assets/img/characters/';
+export const SW_CHARACTERS: string = SWAPI_URL + CHARACTERS;
